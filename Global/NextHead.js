fbce74a86bab273de71372145e30df52f37dec0d
Omar <omaar_5@hotmail.com>
2022-01-01 17:04:49 +0200

    setup public folder and Google Fonts

--- Global/NextHead.js
+++ Global/NextHead.js
@@ -3,7 +3,16 @@ import Head from "next/head";
 export const NextHead = () => {
     return (
         <Head>
-            <title>Landing Page</title>
+            {/* Favicon */}
+            <link rel="shortcut icon" href="/favicon.png" type="image/x-icon" />
+
+            {/* Title */}
+            <title>Landing Page Website</title>
+
+            {/* Google Fonts */}
+            <link rel="preconnect" href="https://fonts.googleapis.com" />
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin={true} />
+            <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700&display=swap" rel="stylesheet" />
         </Head>
     );
 };
